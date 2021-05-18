c83f54acdbdc6772cfc0843f66c4655b2a6af64b
Łukasz Narloch <lukassz111@gmail.com>
2021-05-18 19:49:48 +0200

    files changed by author

--- commandline_scraped_data_from_git_log/scrap_changed_files.sh
+++ commandline_scraped_data_from_git_log/scrap_changed_files.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-git -C ./tensorflow/ ls-files | wc -l >./../data/files_in_repo.txt
+git -C ./tensorflow/ ls-files | wc -l >./../../data/files_in_repo.txt
 echo "Generatring changed_files.txt"
-echo "commith_hash,file" >./../data/changed_files.txt
-cat commits_hash.txt | while read line; do
-  #files_changed=$(git -C ./tensorflow/ diff-tree --no-commit-id --name-only -r $line)
-  echo "commit: ${line}"
-  git -C ./tensorflow/ diff-tree --no-commit-id --name-only -r $line | while read file; do
-    echo "${line},${file}" >>./../data/changed_files.txt
+find . -type f -not -path '*/.git/*' -exec echo {} \; >./../list_files.txt
+#echo "commith_hash,file" >./../data/changed_files.txt
+cat ../list_files.txt | while read line; do
+  #authors=$(git -C ./tensorflow/ log --pretty=format:"%an" $line | sort | uniq -u)
+  echo "file: ${line}"
+  git log --pretty=format:"%an" $line | sort | uniq -u | while read author; do
+    echo "${line},${author}" >>./../../data/changed_files.txt
   done
 done
