40e3801e9087dd36414e546dacb8b8e69d529904
Łukasz Narloch <lukassz111@gmail.com>
2021-05-18 14:03:36 +0200

    commits.csv

--- commandline_scraped_data_from_git_log/scrap.sh
+++ commandline_scraped_data_from_git_log/scrap.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-git clone https://github.com/tensorflow/tensorflow.git
-#git -C shortlog --summary --numbered --email
+if [ -d "./tensorflow" ]; then
+  git -C ./tensorflow/ fetch
+  git -C ./tensorflow/ pull
+else
+  git clone https://github.com/tensorflow/tensorflow.git
+fi
+echo "tree_hash,commith_hash,author_name,author_email,author_timestamp,committer_name,committer_email,committer_timestamp" >./../data/commits.csv
+git -C ./tensorflow/ log --pretty=format:'%T,%H,%an,%ae,%at,%cn,%ce,%ct' --abbrev-commit >>./../data/commits.csv
